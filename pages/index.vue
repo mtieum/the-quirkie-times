c8426821494786c2289f5452ec645e3919b6b8bb
Tieum <mtieum@gmail.com>
2022-08-10 16:04:16 +0100

    change excerpt to list

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <img src="../assets/quirkietimes-pagetop.jpeg" alt="" width="100%" height="100%"/>
       
     </section> -->
-    <section class="mt-8 px-6 max-w-5xl mx-auto">
+    <section class="mt-8 px-6 max-w-7xl mx-auto">
       <!-- <h1 class="text-3xl md:text-5xl lg:text-7xl">COMING SOON</h1> -->
       <h3 class="text-black dark:text-primary-400 max-w-5xl mx-auto">Latest Issues</h3>
       <posts post-type="newsletters" :amount="5" />
