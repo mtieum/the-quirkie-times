4764ab8253e41a45902b8fc8b99551a9345f0e42
Tieum <mtieum@gmail.com>
2022-08-11 16:21:29 +0100

    coming soon

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <main>
     <section class="container px-6 mx-auto">
-      <!-- <h1 class="text-3xl md:text-5xl lg:text-7xl">COMING SOON</h1> -->
-      <h1 class="mt-12 mb-8">Latest Issues</h1>
+      <h1 class="text-3xl md:text-5xl lg:text-7xl">COMING SOON</h1>
+      <!-- <h1 class="mt-12 mb-8">Latest Issues</h1>
       <div class="flex flex-row justify-content-center">
         <div class="grid grid-rows-1 grid-flow-col gap-4">
           <news post-type="newsletters" :amount="3" />
         </div>
-      </div>
+      </div> -->
     </section>
   </main>
 </template>
